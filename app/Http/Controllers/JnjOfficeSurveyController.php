2d667b43b7848645a5373fa65e3ae88cac5a4ff2
Cris <crisjohnreytarpin@gmail.com>
2021-11-05 19:25:23 +0800

    updates

--- app/Http/Controllers/JnjOfficeSurveyController.php
+++ app/Http/Controllers/JnjOfficeSurveyController.php
@@ -23,9 +23,9 @@ class JnjOfficeSurveyController extends Controller
         $data = collect($request->all())->merge(['survey' => $survey[$request->survey], 'num' => $request->survey])->toArray();
         Mail::to($request->a2_2 ?? $request->h2_2)->send(new SurveyCompleted($data));
         $mails = $emails[$request->country];
-        // Mail::to($mails[0])->send(new SurveyCompleted($data));
-        // Mail::to($mails[1])->send(new SurveyCompleted($data));
-        // Mail::to($mails[2])->send(new SurveyCompleted($data));
+        Mail::to($mails[0])->send(new SurveyCompleted($data));
+        Mail::to($mails[1])->send(new SurveyCompleted($data));
+        Mail::to($mails[2])->send(new SurveyCompleted($data));
         return redirect("https://www.seeyourabiliti.com/professionals");
     }
 }
